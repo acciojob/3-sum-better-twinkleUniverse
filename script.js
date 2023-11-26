baa7034c6c14ded14831b3d1d892af32abfa82c4
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-11-26 15:21:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum=arr[0]+[1]+[2];
+	arr.sort((a, b) => a - b);
+	for(let i=0;i<arr.length-2;i++){
+		let j=i+1;
+		let k=arr.length-1;
+		while(j<k){
+         let currSum=arr[i]+arr[j]+arr[k];
+		 if(Math.abs(target-currSum)<Math.abs(target-sum))
+			 sum=currSum;
+		 if(target<arr[j]){
+			 j++;
+		 }else
+			 k--;
+		}
+	}
+	return sum;
+	
 }
 
 module.exports = threeSum;
